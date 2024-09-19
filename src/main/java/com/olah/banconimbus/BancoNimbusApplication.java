6f07802c3d743adcb197b88c30c905ea02574678
Andrea Kelekian <andrea.kelekian@mprj.mp.br>
2024-09-18 22:39:01 -0300

    Commit inicial

---
+++ src/main/java/com/olah/banconimbus/BancoNimbusApplication.java
@@ -0,0 +1,13 @@
+package com.olah.banconimbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoNimbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoNimbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olah/banconimbus/BancoNimbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olah.banconimbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoNimbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
